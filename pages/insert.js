6b02cb10fbc584a649825d59df298a8955c7e7d5
chris.wan <chris.wan@soshk.com>
2023-09-19 01:09:10 +0800

    fixing numPad M and X value

--- pages/insert.js
+++ pages/insert.js
@@ -1,48 +1,42 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
+import "../styles/Home.module.css";
+import { useCallback, useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
-import React, { useCallback, useEffect, useState } from 'react';
-import Container from '@mui/material/Container';
-
-const inter = Inter({ subsets: ['latin'] })
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import Button from "@mui/material/Button";
 
 const roundAttemptInitValues = {
   round: null,
   attempt: null
 };
 
-function Insert() {
+export default function App() {
   const [rounds, setRounds] = useState(6);
+  const [shots, setshots] = useState(6);
   const [gameInfo, setGameInfo] = useState({
     roundScores: []
   });
   const [roundAttemptSelected, setRoundAttemptSelected] = useState(
     roundAttemptInitValues
   );
+  const numPad = ['M',1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'X'];
 
   const [isModalOpen, setModalOpen] = useState(false);
   const handleClose = useCallback(() => {
     setModalOpen(false);
     setRoundAttemptSelected(roundAttemptInitValues);
   }, []);
+
+  useEffect(() => {
+    const initScores = Array.apply(null, Array(rounds)).map(() => {
+      return Array.apply(null, Array(shots)).map(() => 0);
+    });
+    const initGameInfo = {
+      roundScores: initScores
+    };
+    setGameInfo(initGameInfo);
+  }, [rounds]);
 
   const style = {
     position: "absolute",
@@ -56,16 +50,6 @@ function Insert() {
     p: 4
   };
 
-  useEffect(() => {
-    const initScores = Array.apply(null, Array(rounds)).map(() => {
-      return Array.apply(null, Array(6)).map(() => 0);
-    });
-    const initGameInfo = {
-      roundScores: initScores
-    };
-    setGameInfo(initGameInfo);
-  }, [rounds]);
-
   const updateScore = useCallback(
     (round, attempt, mark) => {
       const tGameInfo = { ...gameInfo };
@@ -78,143 +62,137 @@ function Insert() {
     },
     [gameInfo]
   );
+
+  function sumScore(rs) {
+    var _rs = rs;
+    _rs.forEach((element, index) => {
+      if (element === 'M') {
+        _rs[index] = 0;
+      }
+      if (element === 'X') {
+        _rs[index] = 10;
+      }      
+    });
+    return +_rs[0] + _rs[1] + _rs[2];
+  }
+
   return (
-    <>
-      <Head>
-        <title>Insert Archery Record</title>
-        <meta name="description" content="Archery Record" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={darkTheme}>
-        <main className={styles.main}>
-          <Container maxWidth="auto">
-            <h1 align="center">個人分紙</h1>
-            <br /><br />
-            <Box sx={{ display: "flex" }}>
-              <Box sx={{ flex: 3 }}>
+    <div className="App">
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ flex: 3 }}>
+          <Grid container sx={{ marginBottom: 1 }}>
+            <Grid item xs={4}>
+              &nbsp;
+            </Grid>
+            <Grid item xs={4}>
+              &nbsp;
+            </Grid>
+            <Grid item xs={4}>
+              &nbsp;
+            </Grid>
+          </Grid>
+          {gameInfo.roundScores.map((rs, roundIndex) => {
+            return (
+              <Box>
+                <Typography sx={{ textAlign: "left" }}>
+                  Round {roundIndex + 1}
+                </Typography>
                 <Grid container sx={{ marginBottom: 1 }}>
-                  <Grid item xs={4}>
-                    &nbsp;
-                  </Grid>
-                  <Grid item xs={4}>
-                    &nbsp;
-                  </Grid>
-                  <Grid item xs={4}>
-                    &nbsp;
-                  </Grid>
-                </Grid>
-                {gameInfo.roundScores.map((rs, roundIndex) => {
-                  return (
-                    <Box>
-                      <Typography sx={{ textAlign: "left" }}>
-                        Round {roundIndex + 1}
-                      </Typography>
-                      <Grid container sx={{ marginBottom: 1 }}>
-                        {rs.map((s, attemptIndex) => {
-                          return (
-                            <Grid item xs={4}>
-                              <Box border={1}>
-                                <Button
-                                  onClick={() => {
-                                    setModalOpen(true);
-                                    setRoundAttemptSelected({
-                                      round: roundIndex,
-                                      attempt: attemptIndex
-                                    });
-                                  }}
-                                >
-                                  {s}
-                                </Button>
-                              </Box>
-                            </Grid>
-                          );
-                        })}
-                      </Grid>
-                    </Box>
-                  );
-                })}
-              </Box>
-              <Box sx={{ flex: 2 }}>
-                <Grid container sx={{ marginBottom: 1 }}>
-                  <Grid item xs={6} sx={{ backgroundColor: "red" }}>
-                    3
-                  </Grid>
-                  <Grid item xs={6} sx={{ backgroundColor: "red" }}>
-                    6
-                  </Grid>
-                </Grid>
-                {gameInfo.roundScores.map((rs) => {
-                  return (
-                    <Box>
-                      <Typography>&nbsp;</Typography>
-                      <Grid container sx={{ marginBottom: 1 }}>
-                        <Grid item xs={6}>
-                          <Box border={1}>
-                            <Button sx={{ color: "black" }}>
-                              {+rs[0] + rs[1] + rs[2]}
-                            </Button>
-                          </Box>
-                        </Grid>
-                        <Grid item xs={6}></Grid>
-                        <Grid item xs={6}>
-                          <Box border={1}>
-                            <Button sx={{ color: "black" }}>
-                              {+rs[3] + rs[4] + rs[5]}
-                            </Button>
-                          </Box>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Box border={1}>
-                            <Button sx={{ color: "black" }}>
-                              {rs.reduce((prev, curr) => {
-                                return prev + curr;
-                              }, 0)}
-                            </Button>
-                          </Box>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  );
-                })}
-                <Grid container sx={{ marginBottom: 1 }}>
-                  <Grid item xs={6}>
-                    <h3>Total</h3>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Box>
-            <Modal open={isModalOpen} onClose={handleClose}>
-              <Box sx={style}>
-                <Grid container>
-                  {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((mark) => {
+                  {rs.map((s, attemptIndex) => {
                     return (
-                      <Grid item xs={3}>
-                        <Button
-                          variant="outlined"
-                          fullWidth
-                          onClick={() => {
-                            updateScore(
-                              roundAttemptSelected?.round,
-                              roundAttemptSelected?.attempt,
-                              mark
-                            );
-                            handleClose();
-                          }}
-                        >
-                          {mark}
-                        </Button>
+                      <Grid item xs={4}>
+                        <Box border={1}>
+                          <Button
+                            onClick={() => {
+                              setModalOpen(true);
+                              setRoundAttemptSelected({
+                                round: roundIndex,
+                                attempt: attemptIndex
+                              });
+                            }}
+                          >
+                            {s}
+                          </Button>
+                        </Box>
                       </Grid>
                     );
                   })}
                 </Grid>
               </Box>
-            </Modal>
-          </Container>
-        </main>
-      </ThemeProvider>
-    </>
-  )
-}
+            );
+          })}
+        </Box>
+        <Box sx={{ flex: 2 }}>
+          <Grid container sx={{ marginBottom: 1 }}>
+            <Grid item xs={6} sx={{ backgroundColor: "red" }}>
+              Rounds: {rounds}
+            </Grid>
+            <Grid item xs={6} sx={{ backgroundColor: "red" }}>
+              Shots: {shots}
+            </Grid>
+          </Grid>
+          {gameInfo.roundScores.map((rs) => {
+            return (
+              <Box>
+                <Typography>&nbsp;</Typography>
+                <Grid container sx={{ marginBottom: 1 }}>
+                  <Grid item xs={6}>
+                    <Box border={1}>
+                      <Button sx={{ color: "black" }}>
+                        {sumScore(rs)}
+                      </Button>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6}></Grid>
+                  <Grid item xs={6}>
+                    <Box border={1}>
+                      <Button sx={{ color: "black" }}>
+                        {+rs[3] + rs[4] + rs[5]}
+                      </Button>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Box border={1}>
+                      <Button sx={{ color: "black" }}>
+                        {rs.reduce((prev, curr) => {
+                          return prev + curr;
+                        }, 0)}
+                      </Button>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
 
-export default Insert;
+      <Modal open={isModalOpen} onClose={handleClose}>
+        <Box sx={style}>
+          <Grid container>
+            {numPad.map((mark) => {
+              return (
+                <Grid item xs={3}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={() => {
+                      updateScore(
+                        roundAttemptSelected?.round,
+                        roundAttemptSelected?.attempt,
+                        mark
+                      );
+                      handleClose();
+                    }}
+                  >
+                    {mark}
+                  </Button>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
